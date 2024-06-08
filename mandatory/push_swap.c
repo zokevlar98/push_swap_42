81f08565a50b5bbc1b34cd20a923406b6a36c363
Ziad Qouri <zqouri@e1r10p14.1337.ma>
2024-06-08 20:28:40 +0100

    done

--- mandatory/push_swap.c
+++ mandatory/push_swap.c
@@ -6,20 +6,26 @@
 /*   By: zqouri <zqouri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 17:14:41 by zqouri            #+#    #+#             */
-/*   Updated: 2024/06/08 19:41:10 by zqouri           ###   ########.fr       */
+/*   Updated: 2024/06/08 20:09:01 by zqouri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 int	main(int argc, char *argv[])
-
+{
 	t_stack	*a;
-	t_stack *b;
+	t_stack	*b;
+	int		i;
 	
 	// atexit(before_exit);
+	i = 0;
 	if (argc >= 1)
 	{
+		while (i < argc - 1)
+		{
+			
+		}
 		// ft_check_empty()
 	}
 	else
